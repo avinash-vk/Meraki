e411ec3d22d17bd6310d54d27094161ee58503fe
avinash-vk <avinash2000vk@gmail.com>
2020-09-16 17:11:19 +0530

    added Searchbar

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Maps from './Maps';
 import StatusBar from './StatusBar';
+import SearchBar from './SearchBar';
 
 const Home = () => {
   return (
     <div>
       <Maps />
       <StatusBar />
+      <SearchBar />
       You're at home bruh.
     </div>
   );
