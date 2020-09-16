1f4ca294259f5d03054edc209a6f791b6e9a4b50
avinash-vk <avinash2000vk@gmail.com>
2020-09-16 15:45:04 +0530

    added Statusbar

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Maps from './Maps';
+import StatusBar from './StatusBar';
+
 const Home = () => {
   return (
     <div>
       <Maps />
+      <StatusBar />
       You're at home bruh.
     </div>
   );
