9b94f2a219a82dc308a34e88f618fcc43267e034
shreyavenugopal <shrey1010.svg@gmail.com>
2020-09-17 13:36:36 +0530

    Improved Create Story

--- src/views/story/storyCreateForm.js
+++ src/views/story/storyCreateForm.js
@@ -48,21 +48,29 @@ class StoryCreateForm extends React.Component {
 
 		</div>
 
-    <div>
-			<h1 class="formText">Add image/video</h1>
+    <div style={{paddingTop: "5%",paddingLeft: "2%",paddingRight:"2%"}}>
+			<div>
+			<h1 class="formText" style={{float:"left"}}>Add image/video : </h1>
+			<hr style={{color:"white"}}></hr>
 			<InputField
 					type='file'
 					placeholder='Image'
 					value={this.state.image ? this.state.image : ''}
 					onChange={(val) => this.setInputValue('image',val)}
 				/>
-			<h1 class="formText">Target name:</h1>
+			</div>
+			<br></br>
+			<div style={{flexDirection:"column"}}>
+			<h1 class="formText" style={{float:"left"}}>Target name :</h1>
 			<InputField
+					style={{float:"right"}}
 					type='text'
 					placeholder='Name'
 					value={this.state.tname ? this.state.tname : ''}
 					onChange={(val) => this.setInputValue('tname',val)}
 				/>
+			</div>
+			<br></br>
 			<h1 class="formText">What kind of job is the target capable of?</h1>
 			<InputField
 					type='text'
@@ -71,14 +79,14 @@ class StoryCreateForm extends React.Component {
 					onChange={(val) => this.setInputValue('job',val)}
 				/>
 			<h1 class="formText">What is your story?</h1>
-			<InputField
-					style={{width:"100%"}}
+			<textarea
+					class="storyinput"
 					type='story'
 					placeholder='Give a brief desciption of how you met the target'
 					value={this.state.story ? this.state.story : ''}
 					onChange={(val) => this.setInputValue('story',val)}
 				/>
-	<div class="centered">
+	<div class="centered" style={{paddingTop: "20px"}}>
     	<SubmitButton
 					text='Create Story'
 					disable={this.state.buttonDisabled}
