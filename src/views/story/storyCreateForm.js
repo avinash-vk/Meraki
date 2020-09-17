bbd45184bf7d2e0917f2d59b6b6de0c6b9e4dd1d
shreyavenugopal <shrey1010.svg@gmail.com>
2020-09-17 12:41:11 +0530

    Somewhat story form. Width adjestments required

--- src/views/story/storyCreateForm.js
+++ src/views/story/storyCreateForm.js
@@ -1,41 +1,93 @@
 import React from 'react';
-import { 
-  Button,
-  FormGroup,
-  TextField,
-  TextareaAutosize }
-   from '@material-ui/core';
+import InputField   from './InputField';
+import SubmitButton from './SubmitButton';
+import './StoryForm.css';
 
-const StoryCreateForm = () => {
-  return <div>
-    This is the form to fill in to make stories.
+class StoryCreateForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state= {
+			image:'',
+			tname:'',
+			job:'',
+			story:'',
+			buttonDisabled: false
+		}
+	}
+
+	setInputValue(property,val) {
+		val=val.trim();
+		this.setState({
+			[property]: val
+		})
+	}
+
+	resetForm() {
+		this.setState({
+			image:'',
+			tname:'',
+			job:'',
+			story:'',
+			buttonDisabled: false
+		})
+	}
+
+  render(){
+	return( <div>
+    <div class="rectangle">
+
+			<div>
+
+				<div class="ellipse">
+				</div>
+				<div>
+					<h1 class="meraki">Meraki</h1>
+				</div>
+
+			</div>
+
+		</div>
+
     <div>
-    <form>
-      <FormGroup>
-          <label style={{alignSelf:"center"}}> Display Image : {' '} 
-          <input type="file" id="displayImg" label="Display Image"/>
-          </label>
-      </FormGroup>
-      <FormGroup>
-          <label style={{alignSelf:"center"}}> Target Name : {' '}
-          <TextField id="targetName"/>      
-          </label>  
-        </FormGroup>
-        <FormGroup>
-          <label style={{alignSelf:"center"}}> What kind of job can the target do : {' '}
-          <TextField id="targetJob" style={{width:"60vw",alignSelf:"center"}}/>
-          </label>
-        </FormGroup>
-        <FormGroup>
-          <label style={{alignSelf:"center"}}> Target's Story: </label>
-          <TextareaAutosize id="targetStory" placeholder="Story" rowsMin={30}style={{width:"80vw",alignSelf:"center"}}/>
-        </FormGroup>
-      <Button variant="contained" color="primary">
-        Submit
-      </Button>
-      </form>
-      </div>
-  </div>;
+			<h1 class="formText">Add image/video</h1>
+			<InputField
+					type='file'
+					placeholder='Image'
+					value={this.state.image ? this.state.image : ''}
+					onChange={(val) => this.setInputValue('image',val)}
+				/>
+			<h1 class="formText">Target name:</h1>
+			<InputField
+					type='text'
+					placeholder='Name'
+					value={this.state.tname ? this.state.tname : ''}
+					onChange={(val) => this.setInputValue('tname',val)}
+				/>
+			<h1 class="formText">What kind of job is the target capable of?</h1>
+			<InputField
+					type='text'
+					placeholder='Housework/Technician/Cleaning/Security'
+					value={this.state.job ? this.state.job : ''}
+					onChange={(val) => this.setInputValue('job',val)}
+				/>
+			<h1 class="formText">What is your story?</h1>
+			<InputField
+					style={{width:"100%"}}
+					type='story'
+					placeholder='Give a brief desciption of how you met the target'
+					value={this.state.story ? this.state.story : ''}
+					onChange={(val) => this.setInputValue('story',val)}
+				/>
+	<div class="centered">
+    	<SubmitButton
+					text='Create Story'
+					disable={this.state.buttonDisabled}
+					onClick={() => this.doLogin()}
+				/>
+	</div>
+    </div>
+  </div>);
+	}
 };
 
 export default StoryCreateForm;
