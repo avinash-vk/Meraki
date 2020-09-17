d48e5741347a09f5c88de327b40d8bdca8000281
avinash-vk <avinash2000vk@gmail.com>
2020-09-17 17:29:57 +0530

    intermediate changes

---
+++ src/api/index.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://bottlecap.tech/api';
+
+const Test = async () => {
+  let output = '';
+  await axios(`${BASE_URL}`)
+    .then((resp) => {
+      console.log('HERE');
+      output = resp;
+    })
+    .catch((err) => {
+      output = err;
+    });
+  return output;
+};
+
+const CreateStory = async (data) => {
+  // tags = [ carpenter, job needed ]
+  const {uid, imageUrl, title, target, content, tags} = data;
+  const {name, age, gender, contact, location} = target;
+  const {lat, long} = location;
+
+  let output = '';
+
+  const json = JSON.stringify(data);
+
+  await axios
+    .post(`${BASE_URL}/story`, json, {
+      headers: {
+        // Overwrite Axios's automatically set Content-Type
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((resp) => {
+      output = resp;
+    })
+    .catch((err) => console.log('error occured'));
+
+  return output;
+};
+
+export default {
+  Test,
+  CreateStory,
+};
--- src/views/landing/index.js
+++ src/views/landing/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './landing.css';
-import Navbar from './components/Navbar'
-import Contact from './components/Contact'
-import Man from './assets/worker.jpg'
-import { Button } from '@material-ui/core';
-import { withStyles} from '@material-ui/core/styles';
+import Navbar from './components/Navbar';
+import Contact from './components/Contact';
+import Man from './assets/worker.jpg';
+import {Button} from '@material-ui/core';
+import API from '../../api';
+import {withStyles} from '@material-ui/core/styles';
 const ColorButton = withStyles((theme) => ({
   root: {
     color: 'white',
@@ -12,42 +13,109 @@ const ColorButton = withStyles((theme) => ({
     '&:hover': {
       backgroundColor: '#F5564E',
     },
-    fontSize:"4vh",
-   marginLeft:"16%",
-  marginTop:"2%"
+    fontSize: '4vh',
+    marginLeft: '16%',
+    marginTop: '2%',
   },
 }))(Button);
 
 const Landing = () => {
-  return  <div className="container">
-    <div className="headercontainer">
-        <Navbar/>
+  return (
+    <div className="container">
+      <div className="headercontainer">
+        <Navbar />
+
+        <button
+          onClick={async () => {
+            try {
+              const data = {
+                uid: '1234',
+                imageUrl:
+                  'https://images.unsplash.com/photo-1599687349533-82f24a0b62cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+                title: 'Its a great day',
+                target: {
+                  name: 'joe',
+                  age: 20,
+                  gender: 'male',
+                  contact: '874832438247',
+                  location: {
+                    lat: '12.9772793',
+                    long: '77.671645',
+                  },
+                },
+                content: 'it was indeed a great day',
+                tags: ['plumber', 'job needed'],
+              };
+              console.log('Checking', await API.CreateStory(data));
+            } catch (err) {
+              console.log(err);
+            }
+          }}
+        >
+          Click me
+        </button>
         <div>
-        <section className="first-col">
-          <text className="tagline" style={{float:"right",textAlign:"right"}}>We <text className="white-text">Make</text>
-          <br></br>
-          Lives <text className="white-text">Better</text>
+          <section className="first-col">
+            <text
+              className="tagline"
+              style={{float: 'right', textAlign: 'right'}}
+            >
+              We <text className="white-text">Make</text>
+              <br></br>
+              Lives <text className="white-text">Better</text>
+            </text>
+          </section>
+          <section className="second-col">
+            <img src={Man} className="image"></img>
+          </section>
+        </div>
+      </div>
+      <div className="purposecontainer">
+        <div style={{float: 'right', padding: '6vh'}}>
+          <text
+            style={{
+              color: '#F5564E',
+              float: 'right',
+              fontSize: '4vh',
+              fontWeight: 'bold',
+            }}
+          >
+            WHAT DO WE DO.
           </text>
-        </section>
-        <section className="second-col">
-          <img src={Man} className="image"></img>
-        </section>
+          <br></br>
+          <text style={{color: 'white', fontSize: '8vh', float: 'right'}}>
+            Fighting Unemployment
+          </text>
+          <br></br>
+          <text
+            style={{
+              color: '#F5564E',
+              opacity: 0.7,
+              fontSize: '6vh',
+              float: 'right',
+            }}
+          >
+            One story at a time
+          </text>
+          <br></br>
         </div>
+        <div style={{float: 'left', padding: '6vh'}}>
+          <section className="first-col" style={{width: '60%'}}>
+            <text
+              style={{color: 'white', fontSize: '4vh', fontWeight: 'lighter'}}
+            >
+              We believe that no one deserves to have anything short of a basic
+              stable livelihood. And we believe that doing good, is the purest
+              form of humanity.Making a life is something that goes a long way.{' '}
+            </text>
+          </section>
+          <ColorButton variant="contained" size="large">
+            JOIN US
+          </ColorButton>
+        </div>
+      </div>
+      <Contact id="contact" />
     </div>
-    <div className="purposecontainer">
-      <div style={{float:'right',padding:"6vh"}}>
-          <text style={{color:'#F5564E',float:'right',fontSize:"4vh",fontWeight:"bold"}}>WHAT DO WE DO.</text><br></br>
-          <text style={{color:'white',fontSize:"8vh",float:'right'}}>Fighting Unemployment</text><br></br>
-          <text style={{color:'#F5564E',opacity:0.7,fontSize:"6vh",float:'right'}}>One story at a time</text><br></br>
-      </div>
-      <div style={{float:'left',padding:"6vh"}}>
-        <section className="first-col" style={{width:"60%"}}>
-        <text style={{color:'white',fontSize:"4vh",fontWeight:"lighter"}}>We believe that no one deserves to have anything short of a basic stable livelihood. And we believe that doing good, is the purest form of humanity.Making a life is something that goes a long way. </text>
-        </section>
-        <ColorButton variant="contained" size="large">JOIN US</ColorButton>
-      </div>
-        </div>
-        <Contact id="contact"/>
-  </div>
+  );
 };
 export default Landing;
