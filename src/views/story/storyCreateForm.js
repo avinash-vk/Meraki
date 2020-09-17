43226d0a14c69d88fefab26dc3562d3a03d1595b
Sanskriti Pattanayk <sanskriti.pattanayak@gmail.com>
2020-09-17 17:41:53 +0530

    Story form

--- src/views/story/storyCreateForm.js
+++ src/views/story/storyCreateForm.js
@@ -8,9 +8,13 @@ class StoryCreateForm extends React.Component {
 		super(props);
 		this.state= {
 			image:'',
-			tname:'',
-			job:'',
-			story:'',
+      tname:'',
+      tage:'',
+      tgender:'',
+      job:'',
+      title:'',
+      story:'',
+      contact:'',
 			buttonDisabled: false
 		}
 	}
@@ -25,9 +29,14 @@ class StoryCreateForm extends React.Component {
 	resetForm() {
 		this.setState({
 			image:'',
-			tname:'',
-			job:'',
-			story:'',
+      tname:'',
+      tage:'',
+      tgender:'',
+      job:'',
+      title:'',
+      story:'',
+      contact:'',
+
 			buttonDisabled: false
 		})
 	}
@@ -50,18 +59,10 @@ class StoryCreateForm extends React.Component {
 
     <div style={{paddingTop: "5%",paddingLeft: "2%",paddingRight:"2%"}}>
 			<div>
-			<h1 class="formText" style={{float:"left"}}>Add image/video : </h1>
-			<hr style={{color:"white"}}></hr>
-			<InputField
-					type='file'
-					placeholder='Image'
-					value={this.state.image ? this.state.image : ''}
-					onChange={(val) => this.setInputValue('image',val)}
-				/>
 			</div>
 			<br></br>
 			<div style={{flexDirection:"column"}}>
-			<h1 class="formText" style={{float:"left"}}>Target name :</h1>
+			<text class="formText" style={{float:"left"}}>Target name </text>
 			<InputField
 					style={{float:"right"}}
 					type='text'
@@ -69,20 +70,63 @@ class StoryCreateForm extends React.Component {
 					value={this.state.tname ? this.state.tname : ''}
 					onChange={(val) => this.setInputValue('tname',val)}
 				/>
-			</div>
+        	<text class="formText" style={{float:"left"}}>Target Age </text>
+			<InputField
+					style={{float:"right"}}
+					type='number'
+					placeholder='Age'
+					value={this.state.tage ? this.state.tage : ''}
+					onChange={(val) => this.setInputValue('tage',val)}
+				/>
+        </div>
+
+			<text class="formText" style={{float:"left"}}>Target Gender </text>
+			<InputField
+					style={{float:"right"}}
+					type='text'
+					placeholder='Gender'
+					value={this.state.tgender ? this.state.tgender : ''}
+					onChange={(val) => this.setInputValue('tgender',val)}
+				/>
+        	<text class="formText" style={{float:"left"}}>Target Contact </text>
+			<InputField
+					style={{float:"right"}}
+					type='tel'
+					placeholder='Contact No.'
+					value={this.state.contact ? this.state.contact : ''}
+					onChange={(val) => this.setInputValue('contact',val)}
+				/>
 			<br></br>
-			<h1 class="formText">What kind of job is the target capable of?</h1>
+			<text class="formText" style={{float:"left"}}>What kind of job is the target capable of?</text>
 			<InputField
 					type='text'
 					placeholder='Housework/Technician/Cleaning/Security'
 					value={this.state.job ? this.state.job : ''}
-					onChange={(val) => this.setInputValue('job',val)}
+          onChange={(val) => this.setInputValue('job',val)}
+          className="inputfull"
+          style={{float:"right"}}
 				/>
-			<h1 class="formText">What is your story?</h1>
+        	<text class="formText" style={{float:"left"}}>Add Story Cover </text>
+		
+			<InputField
+					type='url'
+					placeholder='Image URL'
+					value={this.state.image ? this.state.image : ''}
+					onChange={(val) => this.setInputValue('image',val)}
+				/>
+        <text class="formText">Quote them</text>
+        <textarea
+					class="storyinput"
+					
+					placeholder='Something they said that made you smile! :)'
+					value={this.state.title ? this.state.title : ''}
+					onChange={(val) => this.setInputValue('title',val)}
+				/>
+			<text class="formText">What is their story?</text>
 			<textarea
 					class="storyinput"
-					type='story'
-					placeholder='Give a brief desciption of how you met the target'
+					
+					placeholder='Give a brief desciption of how you met the target?What their life story is ?'
 					value={this.state.story ? this.state.story : ''}
 					onChange={(val) => this.setInputValue('story',val)}
 				/>
